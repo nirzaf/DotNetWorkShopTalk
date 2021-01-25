17e04c0212f3bc81c6ebf2767d3a82c14ec487ed
Scott Sauber <stsauber@gmail.com>
2021-01-24 23:19:56 -0600

    cleanup startup, unused deps

--- 2021-1-27 - NDC London - Security Headers/src/Startup.cs
+++ 2021-1-27 - NDC London - Security Headers/src/Startup.cs
@@ -1,39 +1,23 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Westwind.AspNetCore.LiveReload;
 
 namespace SecurityHeadersTalk
 {
     public class Startup
     {
         private readonly IWebHostEnvironment _env;
-        private IConfiguration _configuration;
 
-        public Startup(IConfiguration configuration, IWebHostEnvironment env)
+        public Startup(IWebHostEnvironment env)
         {
             _env = env;
-            _configuration = configuration;
         }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages().AddRazorRuntimeCompilation();
-
-            if (_env.IsDevelopment())
-            {
-                services.AddLiveReload();
-            }
+            services.AddRazorPages();
         }
-
         
         public void Configure(IApplicationBuilder app)
         {
@@ -60,7 +44,6 @@ namespace SecurityHeadersTalk
          
             if (_env.IsDevelopment())
             {
-                app.UseLiveReload();
                 app.UseDeveloperExceptionPage();
             }
             else
