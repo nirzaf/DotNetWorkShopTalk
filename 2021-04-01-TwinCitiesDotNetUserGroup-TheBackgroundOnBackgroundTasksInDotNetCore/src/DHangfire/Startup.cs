ce79f769ef6fc174d6f08a66c3525f37a8354f94
Scott Sauber <stsauber@gmail.com>
2021-03-31 21:25:52 -0500

    cleanup

--- 2021-04-01-TwinCitiesDotNetUserGroup-TheBackgroundOnBackgroundTasksInDotNetCore/src/DHangfire/Startup.cs
+++ 2021-04-01-TwinCitiesDotNetUserGroup-TheBackgroundOnBackgroundTasksInDotNetCore/src/DHangfire/Startup.cs
@@ -69,8 +69,6 @@ namespace DHangfire
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            
-            app.UseHangfireDashboard();
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
