bf7b51f441c3a3b5e8c8855c0221a50a57acd9cd
Scott Sauber <stsauber@gmail.com>
2021-10-19 21:58:12 -0500

    add techcon security headers talk

---
+++ 2021-10-20-TechCon-SecurityHeaders/src/Pages/Examples/CrossSiteScripting.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SecurityHeadersTalk.Pages.Examples
+{
+    public class CrossSiteScriptingModel : PageModel
+    {
+        [BindProperty]
+        public string Input { get; set; }
+        
+        public string Message { get; set; }
+
+        public void OnGet(string message)
+        {
+            Message = message;
+        }
+
+        public IActionResult OnPost()
+        {
+            return RedirectToPage(new { message = Input});
+        }
+    }
+}
