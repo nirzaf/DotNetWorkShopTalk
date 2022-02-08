a01e62b86ab4cf73f28139c7bc4982f0bb55cc79
Scott Sauber <stsauber@gmail.com>
2022-02-07 20:16:50 -0600

    add code mash security headers

---
+++ 2022-01-13-CodeMash-SecurityHeaders/src/Pages/Examples/XFrameOptionsTrick.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SecurityHeadersTalk.Pages.Examples
+{
+    public class XFrameOptionsTrickModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
